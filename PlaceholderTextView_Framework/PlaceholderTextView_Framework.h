3382ad6ecdbd8dbb5ab169d8c157206ea56df9ec
EugeneKlyuenkov <dawd>
2022-08-31 10:01:58 +0300

    initial setup

---
+++ PlaceholderTextView_Framework/PlaceholderTextView_Framework.h
@@ -0,0 +1,18 @@
+//
+//  PlaceholderTextView_Framework.h
+//  PlaceholderTextView_Framework
+//
+//  Created by shakuro on 16.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlaceholderTextView_Framework.
+FOUNDATION_EXPORT double PlaceholderTextView_FrameworkVersionNumber;
+
+//! Project version string for PlaceholderTextView_Framework.
+FOUNDATION_EXPORT const unsigned char PlaceholderTextView_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaceholderTextView_Framework/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,17 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '11.0'
+
+use_frameworks!
+
+workspace 'PlaceholderTextView'
+
+target 'PlaceholderTextView_Framework' do
+    project 'PlaceholderTextView_Framework.xcodeproj'
+end
+
+target 'PlaceholderTextView_Example' do
+    project 'PlaceholderTextView_Example.xcodeproj'
+    pod 'Shakuro.CommonTypes', '1.1.4'
+    pod 'SwiftLint', '0.43.1'
+end
